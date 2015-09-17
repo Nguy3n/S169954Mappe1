4e41f86e2b7c31d75c71af1384bf1d3ef4b5ebaf
Nguy3n <nguy3n_@hotmail.com>
2015-09-17 14:32:52 +0200

    next: gjett ordet

--- S169954Mappe1-master/app/src/main/java/com/example/s169954/Spillet.java
+++ S169954Mappe1-master/app/src/main/java/com/example/s169954/Spillet.java
@@ -10,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
@@ -20,6 +21,12 @@ import android.view.Gravity;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class Spillet extends Activity {
 
@@ -30,6 +37,15 @@ public class Spillet extends Activity {
     private String nyOrd;
     private LinearLayout ordLayout;
     private TextView[] charViews;
+
+    private TableRow bokstaver1, bokstaver2, bokstaver3;
+    private BokstavAdapter bokstavAdapter;
+
+    private ImageView[] hengmannDeler;
+    private int antallDeler = 7;
+    private int gjeldendeDel;
+    private int antallChars;
+    private int antallKorrekt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +60,19 @@ public class Spillet extends Activity {
         gjeldendeOrd = "";
 
         ordLayout = (LinearLayout)findViewById(R.id.ordet);
+
+        hengmannDeler = new ImageView[antallDeler];
+        hengmannDeler[0] = (ImageView)findViewById(R.id.galgen);
+        hengmannDeler[1] = (ImageView)findViewById(R.id.hode);
+        hengmannDeler[2] = (ImageView)findViewById(R.id.kroppen);
+        hengmannDeler[3] = (ImageView)findViewById(R.id.arm1);
+        hengmannDeler[4] = (ImageView)findViewById(R.id.arm2);
+        hengmannDeler[5] = (ImageView)findViewById(R.id.bein1);
+        hengmannDeler[6] = (ImageView)findViewById(R.id.bein2);
+
+        bokstaver1 = (TableRow)findViewById(R.id.bokstaver1);
+        bokstaver2 = (TableRow)findViewById(R.id.bokstaver2);
+        bokstaver3 = (TableRow)findViewById(R.id.bokstaver3);
 
         playGame();
     }
@@ -70,6 +99,14 @@ public class Spillet extends Activity {
             charViews[c].setBackgroundResource(R.drawable.bokstav_bg);
 
             ordLayout.addView(charViews[c]);
+        }
+
+        gjeldendeDel = 0;
+        antallChars = gjeldendeOrd.length();
+        antallKorrekt = 0;
+
+        for(int i = 0; i < antallDeler; i++) {
+            hengmannDeler[i].setVisibility(View.INVISIBLE);
         }
     }
 
@@ -117,5 +154,82 @@ public class Spillet extends Activity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void bokstavTrykket(View view) {
+        String bokstav = ((TextView)view).getText().toString();
+        char bokstavChar = bokstav.charAt(0);
+        view.setEnabled(false);
+        view.setBackgroundColor(0xFF000000);
+
+        boolean riktig = false;
+        for (int i = 0; i < gjeldendeOrd.length(); i++) {
+            if(gjeldendeOrd.charAt(i)==bokstavChar) {
+                riktig = true;
+                antallKorrekt++;
+                charViews[i].setTextColor(Color.BLACK);
+            }
+        }
+
+        if(riktig) {
+            if(antallKorrekt == antallChars) {
+                fjernKnapper();
+
+                AlertDialog.Builder vunnetBuild = new AlertDialog.Builder(this);
+                vunnetBuild.setTitle("YESS!");
+                vunnetBuild.setMessage("Du har vunnet!\nRiktig svar var:\n" + gjeldendeOrd);
+                vunnetBuild.setPositiveButton("Spill igjen", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Spillet.this.playGame();
+                    }
+                });
+
+                vunnetBuild.setNegativeButton("Avslutt", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Spillet.this.finish();
+                    }
+                });
+
+                vunnetBuild.show();
+            }
+        }
+        else if(gjeldendeDel < antallDeler) {
+            hengmannDeler[gjeldendeDel].setVisibility(View.VISIBLE);
+            gjeldendeDel++;
+        }
+        else {
+            fjernKnapper();
+
+            AlertDialog.Builder taptBuild = new AlertDialog.Builder(this);
+            taptBuild.setTitle("OH!");
+            taptBuild.setMessage("Du har tapt!\nRiktig svar var\n" + gjeldendeOrd);
+            taptBuild.setPositiveButton("Spill igjen", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    Spillet.this.playGame();
+                }
+            });
+
+            taptBuild.setNegativeButton("Avslutte", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    Spillet.this.finish();
+                }
+            });
+
+            taptBuild.show();
+
+        }
+    }
+
+    public void fjernKnapper() {
+        int antallBokstaver = bokstaver1.getChildCount();
+        antallBokstaver += bokstaver2.getChildCount();
+        antallBokstaver += bokstaver3.getChildCount();
+
+        for (int i = 0; i < antallBokstaver; i++) {
+            bokstaver1.getChildAt(i).setEnabled(false);
+            bokstaver2.getChildAt(i).setEnabled(false);
+            bokstaver3.getChildAt(i).setEnabled(false);
+        }
+
     }
 }
